fc6357cdea1df50d2b260228e84266204db1e3f6
Seyfaro <edenroy@hotmail.co.uk>
2023-03-13 00:55:18 +1000

    Prints table

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+from tabulate import tabulate
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 # Create a new instance of the Firefox driver
 driver = webdriver.Chrome()
+
 
 # Navigate to the website
 driver.get('https://www.fife.gov.uk/services/bin-calendar')
@@ -23,11 +30,51 @@ input_box.send_keys('KY15 5NA')
 # Press Enter to submit the form, or click the search button
 search_button.click()
 
-# Wait for the page to load
-WebDriverWait(driver, 10).until(EC.title_contains('Search Results'))
 
-# Perform actions on the search results page
-# ...
+# Wait for the dropdown menu to load
+time.sleep(4)
+# Wait for the dropdown menu to load
+dropdown_menu = driver.find_element('id', 'dform_widget_ps_3SHSN93_id')
+time.sleep(4)
 
-# Close the browser window
-driver.quit()
+
+# Select an option from the dropdown menu
+
+select = Select(dropdown_menu)
+select.select_by_visible_text('18 MAIN STREET, CERES, FIFE, KY15 5NA')
+
+
+
+# Extract!
+
+time.sleep(4)
+
+div_elements = driver.find_elements(By.CSS_SELECTOR, "div.dform_td[data-name='type']")
+
+
+type_list = []
+
+for div_element in div_elements:
+    text = div_element.text
+    type_list.append(text)
+
+print(type_list)
+
+div_dates = driver.find_elements(By.CSS_SELECTOR, "div.dform_td[data-name='date']")
+
+
+date_list = []
+
+for div_date in div_dates:
+    text = div_date.text
+    date_list.append(text)
+
+print(date_list)
+
+ThisWeek = [[date_list[i], type_list[i]] for i in range(len(date_list))]
+
+print(ThisWeek)
+
+
+# Print the 2D array as a table
+print(tabulate(ThisWeek, headers=['Collection Date', 'Bin Type'], tablefmt='orgtbl'))
